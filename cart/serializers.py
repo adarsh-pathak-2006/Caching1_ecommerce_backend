86610b9412fe48563fb260ef9a9429fd16e60850
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-27 05:09:57 +0530

    added throtttlin and ratelimitinfg in the views and also api views in carts

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,17 +1,33 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from cart.models import Cart, CartItem
 from accounts.serializers import ProfileGetSerializer
+from products.models import Product
+from products.serializers import ProductGetSerializer
 
 
-class CartItemSerializer(ModelSerializer):
+class CartItemwriteSerializer(ModelSerializer):
+    product=PrimaryKeyRelatedField(Product.objects.all())
     class Meta:
         model=CartItem
         fields='__all__'
         read_only_fields=['total_price']
 
+class CartItemGetSerializer(ModelSerializer):
+    product=ProductGetSerializer(read_only=True)
+    class Meta:
+        model=CartItem
+        fields='__all__'
+
+class CartItemUpdateSerializer(ModelSerializer):
+    product=ProductGetSerializer(read_only=True)
+    class Meta:
+        model=CartItem
+        fields=['cart', 'product', 'quantity']
+        read_only_fields=['cart']
+
 class CartSerializers(ModelSerializer):
     profile=ProfileGetSerializer(read_only=True)
-    cart_items=CartItemSerializer(read_only=True, many=True)
+    cart_items=CartItemGetSerializer(read_only=True, many=True)
     class Meta:
         model=Cart
         fields='__all__'
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from cart.models import Cart, CartItem
+from cart.serializers import CartItemwriteSerializer, CartSerializers, CartItemGetSerializer, CartItemUpdateSerializer
+from e_commerce.throttle import GeneralAPIsThrottle
+from rest_framework.permissions import IsAuthenticated
+from accounts.models import Profile
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CartAPI(APIView):
+    throttle_classes=[GeneralAPIsThrottle]
+    permission_classes=[IsAuthenticated]
+    def get(self, request):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        cart_data=get_object_or_404(Cart, profile=profile_data)
+        serial=CartSerializers(cart_data)
+        return Response(serial.data, status=200)
+
+class CartItemAPI(APIView):
+    throttle_classes=[GeneralAPIsThrottle]
+    permission_classes=[IsAuthenticated]
+    def get(self, request):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        cart_data=get_object_or_404(Cart, profile=profile_data)
+        data=CartItem.objects.filter(cart=cart_data)
+        serial=CartItemGetSerializer(data, many=True)
+        return Response(serial.data, status=200)
+
+    def post(self, request):
+        serial=CartItemwriteSerializer(data=request.data)
+        if serial.is_valid():
+            profile_data=get_object_or_404(Profile, user=self.request.user)
+            cart_data=get_object_or_404(Cart, profile=profile_data)
+            serial.save(cart=cart_data)
+            return Response(serial.data, status=201)
+        else:
+            return Response(serial.errors, status=400)
+
+    def put(self, request, pk):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        cart_data=get_object_or_404(Cart, profile=profile_data)
+        instance=get_object_or_404(CartItem, id=pk, cart=cart_data)
+        serial=CartItemUpdateSerializer(instance, data=request.data, partial=True)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data, status=200)
+        else:
+            return Response(serial.errors, status=400)
+
+    def delete(self, request, pk):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        cart_data=get_object_or_404(Cart, profile=profile_data)
+        instance=get_object_or_404(CartItem, id=pk, cart=cart_data)
+        instance.delete()
+        return Response({'message':'cart item deleted'}, status=204)
+
+
+           
+    
+    
