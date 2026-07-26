5a908a03bade4c0cecea8b895f9620df1401ba2a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 13:33:52 +0530

    added category and product serializers

---
+++ products/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from products.models import Category, Product
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model=Category
+        fields=['name', 'slug', 'decription', 'image', 'is_active', 'created_at']
+
+class ProductSerializer(ModelSerializer):
+    category=CategorySerializer(read_only=True)
+    class Meta:
+        model=Product
+        fields='__all__'
