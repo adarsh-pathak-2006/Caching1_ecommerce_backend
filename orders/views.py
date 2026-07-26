ef44ed34161eed556cd8e06182e84b3a7cfcaccd
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 19:53:50 +0530

    added order api views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from orders.models import OrderItem, Order
+from orders.serializers import OrderSerializer, OrderItemSerializer, OrderGetSerializer
+from rest_framework.views import APIView
+from accounts.models import Profile
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+
+class MyOrderAPI(ListCreateAPIView):
+    serializer_class=OrderSerializer
+    def get_queryset(self):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        return Order.objects.filter(user=profile_data)
+
+    def perform_create(self, serializer):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        serializer.save(user=profile_data)
+
+class MyOrderAPIIndividual(RetrieveAPIView):
+    serializer_class=OrderSerializer
+
+    def get_queryset(self):
+        profile_data=get_object_or_404(Profile, user=self.request.user)
+        return Order.objects.filter(user=profile_data)
+
+class AllOrdersAPI(ListAPIView):
+    queryset=Order.objects.all()
+    serializer_class=OrderSerializer
+
+class AllOrderAPIIndividual(RetrieveUpdateDestroyAPIView):
+    queryset=Order.objects.all()
+    serializer_class=OrderSerializer
