da4e0e909feffd98000bac9775937921b5c7cb62
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 12:55:59 +0530

    added drf spectacular documentation integration

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'accounts', 'cart',
     'core', 'orders',
     'products', 'review',
@@ -53,6 +54,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Directs DRF to use drf-spectacular for generating schemas
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Automated API documentation via OpenAPI 3',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 ROOT_URLCONF = 'e_commerce.urls'
 
