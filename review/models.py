e13a85e7149a1d513928d59ecc8541db9b8f1281
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 12:49:17 +0530

    added review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Product
+from accounts.models import Profile
 
-# Create your models here.
+class Review(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    rating=models.CharField(max_length=6, choices=[('1 STAR', '1 Star'), ('2 STAR', '2 Star'), ('3 STAR', '3 Star'), ('4 STAR', '4 Star'), ('5 STAR', '5 Star')])
+    review=models.CharField(max_length=500, blank=True, null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
