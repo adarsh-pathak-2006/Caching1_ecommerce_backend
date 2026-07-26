1784e83bb56adca2fad260927dd54b724eb728cd
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 12:34:20 +0530

    added product models

--- products/models.py
+++ products/models.py
@@ -14,3 +14,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+    slug=models.SlugField()
+    description=models.TextField()
+    price=models.PositiveIntegerField()
+    stock=models.PositiveIntegerField()
+    image=models.ImageField(upload_to='product_images/', blank=True, null=True)
+    brand=models.CharField(max_length=50)
+    sku=models.CharField(max_length=50)
+    is_avaliable=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
