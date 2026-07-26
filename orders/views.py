33337ca77773af8707c11fb4a449d09b8b5dd946
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-27 04:34:32 +0530

    added seperate view for ordering

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import get_object_or_404
 from orders.models import Order
 from orders.serializers import OrderSerializer
 from accounts.models import Profile
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from e_commerce.permissions import IsAdmin
 from e_commerce.throttle import GeneralAPIsThrottle
+from django.db import models
+from cart.models import Cart, CartItem
+from rest_framework.response import Response
 
 
 class MyOrderAPI(ListAPIView):
@@ -16,9 +20,20 @@ class MyOrderAPI(ListAPIView):
         profile_data=get_object_or_404(Profile, user=self.request.user)
         return Order.objects.filter(user=profile_data)
 
-    def perform_create(self, serializer):
-        profile_data=get_object_or_404(Profile, user=self.request.user)
-        serializer.save(user=profile_data)
+class OrderCreationAPI(APIView):
+    def post(self, request):
+        profile_data=get_object_or_404(Profile, user=request.user)
+        cart_data=get_object_or_404(Cart, profile=profile_data)
+        serial=OrderSerializer(data=request.data)
+        if serial.is_valid():
+            cartitem=CartItem.objects.filter(cart=cart_data)
+            total_cart_price=cartitem.aggregate(total_price=models.Sum('total_price'))
+            tax=serial.validated_data['tax']
+            shipping_charges=serial.validated_data['shipping_charges']
+            serial.save(user=profile_data, cart=cart_data, final_amout=total_cart_price['total_price']+tax+shipping_charges)
+            return Response(serial.data, status=201)
+        else:
+            return Response(serial.errors, status=400)                
 
 class MyOrderAPIIndividual(RetrieveAPIView):
     throttle_classes=[GeneralAPIsThrottle]
