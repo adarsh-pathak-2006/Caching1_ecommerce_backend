4aae58df2f3604ba9603716e59239c018ded8d69
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-26 20:29:09 +0530

    added api views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from products.models import Category, Product
+from products.serializers import CategorySerializer, ProductSerializer
+from products.models import Product, Category
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+class CategoryAPI(ListCreateAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+
+class CategoryIndividualAPI(RetrieveUpdateDestroyAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+    lookup_field='slug'
+
+class ProductAPI(ListCreateAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+class ProductIndividualAPI(RetrieveUpdateDestroyAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    lookup_field='slug'
