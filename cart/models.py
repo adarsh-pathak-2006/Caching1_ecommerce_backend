23d01345000ebb5dbc3a654031e2a8b3108eadc6
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-27 02:35:29 +0530

    added cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import Profile
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    profile=models.OneToOneField(Profile, on_delete=models.CASCADE)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.profile.user.username
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField()
+    price=models.PositiveIntegerField()
+    total_price=models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product.name
