b076ee9b22914c52a53fb84ea9c00694242e0a80
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-03 22:37:24 +0530

    added consumers properly

--- e_commerce/consumers.py
+++ e_commerce/consumers.py
@@ -1,27 +1,29 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-
+from core.models import Notification
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user=self.scope["user"]
         if await self.user.is_authenticated():
-            self.group_name=f"user_{self.user.id}"
+            self.group_name="users"
             await self.channel_layer.group_add(self.channel_name, self.group_name)
             await self.connect()
         else:
             await self.close()
 
     async def receive(self, text_data):
-        data=await json.loads(text_data)
-        message=await data.get('message')
-        payload={'type':'notification', 'message':message}
-        await self.channel_layer.group_send(self.group_name, payload)
+        notification_data=await Notification.objects.alast()
+        if notification_data:
+            title=notification_data.title
+            content=notification_data.content
+            await self.channel_layer.group_send(self.group_name, {'type':'notification', 'title':title, 'content':content})
 
-    async def notification(self, event):
-        message=event['message']
-        await self.send(text_data=await json.dumps({'type':'notification', 'message':message}))
+    async def notification_message(self, event):
+        title=event['title']
+        content=event['content']
+        await self.send(text_data=json.dumps({'type':'notification', 'title':title, 'content':content}))
 
     async def disconnect(self, code):
-        self.channel_layer.group_discard(self.group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
         
