392ad5bf6fcaf511e721ddfca16e5291f35f6991
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-03 22:04:46 +0530

    added notification model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Notification(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    time=models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
