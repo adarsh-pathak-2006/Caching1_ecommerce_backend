9e824c3e90ccea381b6938b2ff116234a7b55f1a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-03 20:11:00 +0530

    wired the individual app apis to the main project apis

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -25,6 +25,10 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('accountapis/', include('accounts.urls')),
+    path('cartapis/', include('cart.urls')),
+    path('productapis/', include('products.urls')),
+    path('reviewapis/', include('review.urls')),
 ]
 
 if settings.DEBUG:
