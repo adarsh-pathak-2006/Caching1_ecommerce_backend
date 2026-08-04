23fc7171590fd7ea75ba68523157414a4e85f2e4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-04 12:25:45 +0530

    added caching

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -153,3 +153,10 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
+}
--- products/views.py
+++ products/views.py
@@ -1,50 +1,118 @@
+from django.shortcuts import get_object_or_404
 from products.models import Category, Product
 from products.serializers import CategorySerializer, ProductSerializer
 from products.models import Product, Category
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from e_commerce.permissions import IsAdmin
 from e_commerce.throttle import GeneralAPIsThrottle
+from django.core.cache import cache
+from rest_framework.response import Response
 
 
-class CategoryAPI(ListCreateAPIView):
+class CategoryAPI(APIView):
     throttle_classes=[GeneralAPIsThrottle]
     def get_permissions(self):
         if self.request.method=="GET":
             return [IsAuthenticated()]
         else:
             return [IsAdmin()]
-    queryset=Category.objects.all()
-    serializer_class=CategorySerializer
 
-class CategoryIndividualAPI(RetrieveUpdateDestroyAPIView):
+    def get(self, request):
+        cached_data=cache.get("categories")
+        if cached_data:
+            return Response(cached_data, status=200)
+        data=Category.objects.all()
+        serial=CategorySerializer(data, many=True)
+        return Response(serial.data, status=200)
+
+    def post(self, request):
+        serial=CategorySerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            cache.set("categories", serial.data, timeout=500)
+            return Response(serial.data, status=201)
+        else:
+            return Response(serial.errors, status=400)
+
+    
+class CategoryIndividualAPI(APIView):
     throttle_classes=[GeneralAPIsThrottle]
     def get_permissions(self):
         if self.request.method=="GET":
             return [IsAuthenticated()]
         else:
             return [IsAdmin()]
-    queryset=Category.objects.all()
-    serializer_class=CategorySerializer
-    lookup_field='slug'
 
-class ProductAPI(ListCreateAPIView):
+    def get(self, request, slug):
+        cached_data=cache.get(f"category_{slug}")
+        if cached_data:
+            return Response(cached_data, status=200)
+        data=get_object_or_404(Category, slug=slug)
+        serial=CategorySerializer(data)
+        return Response(serial, status=200)
+
+    def patch(self, request, slug):
+        instance=get_object_or_404(Category, slug=slug)
+        serial=CategorySerializer(instance, data=request.data, partial=True)
+        if serial.is_valid():
+            cache.delete(f"category_{slug}")
+            serial.save()
+            return Response(serial.data)
+
+    def delete(self, request, slug):
+        data=get_object_or_404(Category, slug=slug)
+        data.delete()
+        cache.delete(f"category_{slug}")
+
+class ProductAPI(APIView):
     throttle_classes=[GeneralAPIsThrottle]
     def get_permissions(self):
         if self.request.method=="GET":
             return [IsAuthenticated()]
         else:
             return [IsAdmin()]
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+    def get(self, request):
+        cached_data=cache.get("products")
+        if cached_data:
+            return Response(cached_data, status=200)
+        data=Product.objects.all()
+        serial=ProductSerializer(data, many=True)
+        return Response(serial.data, status=200)
 
-class ProductIndividualAPI(RetrieveUpdateDestroyAPIView):
+    def post(self, request):
+        serial=ProductSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            cache.set("products", serial.data, timeout=500)
+            return Response(serial.data, status=201)
+        else:
+            return Response(serial.errors, status=400)
+
+class ProductIndividualAPI(APIView):
     throttle_classes=[GeneralAPIsThrottle]
     def get_permissions(self):
         if self.request.method=="GET":
             return [IsAuthenticated()]
         else:
             return [IsAdmin()]
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
-    lookup_field='slug'
+    def get(self, request, slug):
+        cached_data=cache.get(f"product_{slug}")
+        if cached_data:
+            return Response(cached_data, status=200)
+        data=get_object_or_404(Product, slug=slug)
+        serial=ProductSerializer(data)
+        return Response(serial, status=200)
+
+    def patch(self, request, slug):
+        instance=get_object_or_404(Product, slug=slug)
+        serial=ProductSerializer(instance, data=request.data, partial=True)
+        if serial.is_valid():
+            cache.delete(f"product_{slug}")
+            serial.save()
+            return Response(serial.data)
+
+    def delete(self, request, slug):
+        data=get_object_or_404(Product, slug=slug)
+        data.delete()
+        cache.delete(f"product_{slug}")
